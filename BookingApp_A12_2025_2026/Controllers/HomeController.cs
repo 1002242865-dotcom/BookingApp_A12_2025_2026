5a33aa3e95f05ed4088d077536b404de04495e53
user <user@T12>
2025-09-28 09:10:01 +0300

    Add project files.

---
+++ BookingApp_A12_2025_2026/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using BookingApp_A12_2025_2026.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingApp_A12_2025_2026.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ////
+            var gemini = new GeminiService();
+            List<Student> students = Student.GetDemoStudents();
+            var listText = string.Join("\n- ", students);
+            var prompt = $"هذه قائمة طلاب:\n- {listText}\n\nهل يمكنك ترتيبهم أبجدياً حسب الاسم وعرضهم في جدول؟";
+           prompt = "write short story in arabic for 4th class";
+            //prompt = "just the final outpot int x=10; int y=9; c.wl(x*y);";
+            //prompt = "just the final number: كم كالوري في ساندويش فلافل";
+            //prompt = "just the final number: كم كالوري في صحن كبير من مقلوبة فلسطينية";
+            //prompt = "show just the final number of calories: كم كالوري في باجيت متوسط مع 250 غرام شوراما";
+            //prompt = "no details' just show th final amount of calories: شربت كاس نسكافيه مكون من 200 ملم حليب وملعقتين نس وملعقة عسل صغيرة";
+
+
+
+            var result = await gemini.AskAsync(prompt);
+
+            ViewBag.Prompt = prompt;
+            ViewBag.Response = result;
+            ////
+
+            Student st1 = new Student
+            {
+                Id = 100,
+                Name = "Omar",
+                Sora="Photos/st11.png"
+            };
+
+            ViewBag.st1 = st1;
+
+              return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Index1()
+        {
+            return View();
+        }
+
+        public IActionResult ShowStudents()
+        {
+
+            List<Student> students = Student.GetDemoStudents();
+            ViewBag.students = students;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BookingApp_A12_2025_2026/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace BookingApp_A12_2025_2026.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Sora { get; set; }
+
+        public override string ToString()
+        {
+            return "ID:"+Id+", Name:"+Name;
+        }
+
+        public static List<Student> GetDemoStudents()
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Id = 1, Name = "Kareem", Sora = "Photos/a1.png" });
+            students.Add(new Student { Id = 2, Name = "Hoda", Sora = "Photos/a2.png" });
+            students.Add(new Student { Id = 3, Name = "Hosni", Sora = "Photos/a3.png" });
+            students.Add(new Student { Id = 4, Name = "Aya", Sora = "Photos/a4.png" });
+            students.Add(new Student { Id = 5, Name = "Montaha", Sora = "Photos/a5.png" });
+            students.Add(new Student { Id = 6, Name = "Mohamed", Sora = "Photos/a1.png" });
+            students.Add(new Student { Id = 7, Name = "Wa3d", Sora = "Photos/a2.png" });
+            students.Add(new Student { Id = 8, Name = "Zohdi", Sora = "Photos/a3.png" });
+            students.Add(new Student { Id = 9, Name = "Noor", Sora = "Photos/a4.png" });
+            students.Add(new Student { Id = 10, Name = "Fairoz", Sora = "Photos/a5.png" });
+            return students;
+        }
+    }
+}
